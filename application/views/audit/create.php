f14fd94a3f19aad0364baad6caffff7b02e332d6
paul <pavel.isupov@mail.ru>
2021-09-14 23:33:36 +0300

    Подправил форму create

--- application/views/audit/create.php
+++ application/views/audit/create.php
@@ -3,61 +3,63 @@
 <?php echo form_open('audit/create'); ?>
 <div class="container col-md-8">
 
-    <h1><?php echo $title ?></h1>
-
-    <div class="row ">
-
-        <div class="form-group">
-            <label for="business_sel">Выбрать СМП</label>
-<!--            <input class="form-control" id="business_sel" name="business_name">-->
-            <select class="form-control" id="business_sel" name="business_name">
-                <?php foreach ($business as $business_item): ?>
-                    <option> <?php echo $business_item['name']; ?></option>
-                <?php endforeach; ?>
-            </select>
-            <label for="supervisor_sel">Выбрать проверяющий орган</label>
-            <select class="form-control" id="supervisor_sel" name="supervisor_name">
-            <?php foreach ($supervisors as $supervisor_item): ?>
-                <option> <?php echo $supervisor_item['name']; ?></option>
-            <?php endforeach; ?>
-            </select>
+    <div class="card " style="width: 20rem;">
+        <div class="card-header text-white flex-fill bg-primary">
+            <h5 class="card-title "><?php echo $title ?></h5>
         </div>
+        <div class="card-body">
+
+            <div class="list-group ">
+                <label for="business_sel">Выбрать СМП</label>
+                <select class="custom-select" id="business_sel" name="business_name">
+                    <?php foreach ($business as $business_item): ?>
+                        <option> <?php echo $business_item['name']; ?></option>
+                    <?php endforeach; ?>
+                </select>
+                <label for="supervisor_sel">Выбрать проверяющий орган</label>
+                <select class="custom-select" id="supervisor_sel" name="supervisor_name">
+                    <?php foreach ($supervisors as $supervisor_item): ?>
+                        <option> <?php echo $supervisor_item['name']; ?></option>
+                    <?php endforeach; ?>
+                </select>
+            </div>
 
 
-    </div>
 
-    <div class="row">
-        <div class="col">
-            <label for="start_date">Дата начала</label><br/>
-            <input type="date"
-                   name="start_date"
-                   id="start_date"
-                   class="form-group"
-                   value="<?php echo set_value('start_date'); ?>"
-            />
-        </div>
-    </div>
+            <div class="row">
+                <div class="col">
+                    <label for="start_date">Дата начала</label><br/>
+                    <input type="date"
+                           name="start_date"
+                           id="start_date"
+                           class="form-group"
+                           value="<?php echo set_value('start_date'); ?>"
+                    />
+                </div>
+            </div>
 
-    <div class="row">
-        <div class="col">
-            <label for="end_date">Дата завершения</label><br/>
-            <input type="date"
-                   name="end_date"
-                   id="end_date"
-                   class="form-group"
-                   value="<?php echo set_value('end_date'); ?>"
-            />
-        </div>
-    </div>
+            <div class="row">
+                <div class="col">
+                    <label for="end_date">Дата завершения</label><br/>
+                    <input type="date"
+                           name="end_date"
+                           id="end_date"
+                           class="form-group"
+                           value="<?php echo set_value('end_date'); ?>"
+                    />
+                </div>
+            </div>
 
 
-    <div class="row">
-        <div class="col">
-            <input type="submit"
-                   name="submit"
-                   class="btn btn-success"
-                   value="Добавить"
-            />
+            <div class="row">
+                <div class="col">
+                    <input type="submit"
+                           name="submit"
+                           class="btn btn-success"
+                           value="Добавить"
+                    />
+                </div>
+            </div>
         </div>
     </div>
 </div>
